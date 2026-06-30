7e95506cd803af25c26c63d3174409a9dd6b461e
AIDILYOWIE <aidilyowie07@gmail.com>
2026-06-30 19:57:13 +0700

    :sparkles: Add fungsi shipping cost

--- lib/utils/helpers/pricing_calculator.dart
+++ lib/utils/helpers/pricing_calculator.dart
@@ -4,7 +4,7 @@ class EPricingCalculator {
   double taxRate = getTaxRateLocation(location);
   double taxAmount = productPrice * taxRate;
 
-  double shippingCost = getShippingsCost(location);
+  double shippingCost = getShippingCost(location);
 
   double totalPrice = productPrice + taxAmount + shippingCost;
   return totalPrice;
@@ -12,7 +12,7 @@ class EPricingCalculator {
 
  // calculate shipping cost
  static double calculateShippingCost(double productPrice, String location) {
-  double shippingCost = getShippingsCost(location);
+  double shippingCost = getShippingCost(location);
   return shippingCost.toStringAsFixed(2);
   }
 
@@ -28,5 +28,12 @@ class EPricingCalculator {
     // return the appropriate tax rate
 
     return 0.10; // example tax rate of 10%
+  }
+
+  static double getShippingCost(String location) {
+    // lookup the shipping cost for the given location from a shipping cost database or API
+    // calculate shipping cost based on various factors like distance, weight, etc.
+
+    return 5.00; // example shipping cost of $5.00
   }
 }
