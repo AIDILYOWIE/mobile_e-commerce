b5d3c466f83f77ed9c54e86d3c602e237cddffe3
AIDILYOWIE <aidilyowie07@gmail.com>
2026-06-30 20:31:39 +0700

    :sparkles: Add validation email

---
+++ lib/utils/validators/validations.dart
@@ -0,0 +1,19 @@
+class EValidatior {
+  
+  static String? validationEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+
+    // Regular expression for email validation
+    final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Validation email address';
+    }
+
+    return null;
+   }
+
+
+}
