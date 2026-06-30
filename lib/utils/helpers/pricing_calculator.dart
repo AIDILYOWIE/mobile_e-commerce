07dc15eb56258337aa76c7431be14ffbfa958f93
AIDILYOWIE <aidilyowie07@gmail.com>
2026-06-30 19:47:27 +0700

    :sparkles: Add helpers Pricing Calculator and Add function calculate total price

--- lib/utils/helpers/pricing_calculator.dart
+++ lib/utils/helpers/pricing_calculator.dart
@@ -1,4 +1,12 @@
 class EPricingCalculator {
  // calculate price based on tax and shipping
- static double calculateTotalPrice()
+ static double calculateTotalPrice(double productPrice, String location) {
+  double taxRate = getTaxRateLocation(location);
+  double taxAmount = productPrice * taxRate;
+
+  double shippingCost = getShippingsCost(location);
+
+  double totalPrice = productPrice + taxAmount + shippingCost;
+  return totalPrice;
+ }
 }
