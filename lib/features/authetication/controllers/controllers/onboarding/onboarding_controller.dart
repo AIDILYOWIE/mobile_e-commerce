8f3cca20f92f89463a43c3284b32507f218d6368
AIDILYOWIE <aidilyowie07@gmail.com>
2026-07-02 21:26:06 +0700

    :package: :bug: update file and fix bug

---
+++ lib/features/authetication/controllers/controllers/onboarding/onboarding_controller.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:mobile_ecommerce/features/authetication/controllers/screens/login/login.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  // Variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
+
+  // Updates current index when page scroll
+  void updatePageIndicator(index) => currentPageIndex.value = index;
+
+  // Jump to the specific dot selected page.
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpToPage(index);
+  }
+
+  // Update current index & jump to next page
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      Get.offAll(const LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
+
+  // Update current index & jump to the last page
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
+
+}
--- lib/features/authetication/controllers/screens/onboarding/onboarding.dart
+++ lib/features/authetication/controllers/screens/onboarding/onboarding.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
-import 'package:mobile_ecommerce/features/authetication/controllers/controllers.onboarding/onboarding_controller.dart';
-import 'package:mobile_ecommerce/features/authetication/controllers/screens/widgets/onbaording_dot_navigation.dart';
-import 'package:mobile_ecommerce/features/authetication/controllers/screens/widgets/onboarding_next_button.dart';
+import 'package:mobile_ecommerce/features/authetication/controllers/controllers/onboarding/onboarding_controller.dart';
+import 'package:mobile_ecommerce/features/authetication/controllers/screens/onboarding/widgets/onbaording_dot_navigation.dart';
+import 'package:mobile_ecommerce/features/authetication/controllers/screens/onboarding/widgets/onboarding_next_button.dart';
 import 'package:mobile_ecommerce/utils/constants/image_strings.dart';
-import 'package:mobile_ecommerce/utils/constants/sizes.dart';
 import 'package:mobile_ecommerce/utils/constants/text_strings.dart';
 import 'package:mobile_ecommerce/utils/device/device_utility.dart';
 import 'package:mobile_ecommerce/utils/helpers/helper_functions.dart';
 
-import '../widgets/onbaording_skip.dart';
-import '../widgets/onboarding_page.dart';
+import 'widgets/onbaording_skip.dart';
+import 'widgets/onboarding_page.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
