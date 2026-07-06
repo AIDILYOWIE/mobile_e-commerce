005d6178441bcbfa36c835408bb796e57174ad34
AIDILYOWIE <aidilyowie07@gmail.com>
2026-07-06 21:04:39 +0700

    :lipstick: Add update controller navigation bar

---
+++ lib/navigation_menu.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:mobile_ecommerce/utils/constants/colors.dart';
+import 'package:mobile_ecommerce/utils/helpers/helper_functions.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+    final darkMode = EHelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          backgroundColor: darkMode ? EColors.black : EColors.white,
+          onDestinationSelected: (index) => controller.selectedIndex.value = index,
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
+        ),
+      ),
+      body: Obx(() =>  controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    Center(child: Text('Home Screen')),
+    Center(child: Text('Store Screen')),
+    Center(child: Text('Wishlist Screen')),
+    Center(child: Text('Profile Screen')),
+  ];
+}
