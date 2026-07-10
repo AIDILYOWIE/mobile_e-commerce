9ec4a1e55615e0251c6bf0bb206d34a52d18f25b
AIDILYOWIE <aidilyowie07@gmail.com>
2026-07-10 08:09:06 +0700

    :white_check_mark: Add margin circular_container

--- lib/common/widgets/costume_shapes/containers/circular_container.dart
+++ lib/common/widgets/costume_shapes/containers/circular_container.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:mobile_ecommerce/utils/constants/colors.dart';
 
 class ECircularContainer extends StatelessWidget {
-  const ECircularContainer({super.key, this.width = 400, this.height = 400, this.radius = 400, this.padding = 0, this.child, this.backgroundColor = EColors.white });
+  const ECircularContainer({super.key, this.width = 400, this.height = 400, this.radius = 400, this.padding = 0, this.margin, this.child, this.backgroundColor = EColors.white });
 
   final double? width;
   final double? height;
   final double radius;
   final double padding;
+  final EdgeInsets? margin;
   final Widget? child;
   final Color backgroundColor;
 
@@ -16,6 +17,7 @@ class ECircularContainer extends StatelessWidget {
     return Container(
       width: width,
       height: height,
+      margin: margin,
       padding:  EdgeInsets.all(padding),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(radius),
