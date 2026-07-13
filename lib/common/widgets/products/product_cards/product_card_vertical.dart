7c379fe790194711c7af6ea35135371da430f311
AIDILYOWIE <aidilyowie07@gmail.com>
2026-07-13 19:12:37 +0700

    :white_check_mark: Update text ini product card vertical

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -4,6 +4,9 @@ import 'package:mobile_ecommerce/common/styles/shadows.dart';
 import 'package:mobile_ecommerce/common/widgets/costume_shapes/containers/rounded_container.dart';
 import 'package:mobile_ecommerce/common/widgets/icons/e_circular_icon.dart';
 import 'package:mobile_ecommerce/common/widgets/images/e_rounded_image.dart';
+import 'package:mobile_ecommerce/common/widgets/texts/e_brand_title_text.dart';
+import 'package:mobile_ecommerce/common/widgets/texts/e_brand_title_witb_verified_icon.dart';
+import 'package:mobile_ecommerce/common/widgets/texts/product_price.dart';
 import 'package:mobile_ecommerce/common/widgets/texts/product_title.dart';
 import 'package:mobile_ecommerce/utils/constants/colors.dart';
 import 'package:mobile_ecommerce/utils/constants/image_strings.dart';
@@ -17,10 +20,9 @@ class EProductCardVertical extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = EHelperFunctions.isDarkMode(context);
     return GestureDetector(
-      onTap: (){},
+      onTap: () {},
       child: Container(
         width: 160,
-        padding: const EdgeInsets.all(1),
         decoration: BoxDecoration(
           boxShadow: [EShadowStyle.verticalProductShadow],
           borderRadius: BorderRadius.circular(ESizes.productImageRadius),
@@ -31,7 +33,7 @@ class EProductCardVertical extends StatelessWidget {
             ERoundedContainer(
               height: 160,
               padding: const EdgeInsets.all(ESizes.xs),
-              backgroundColor: dark ? EColors.dark : EColors.white,
+              backgroundColor: Colors.transparent,
               child: Stack(
                 children: [
                   const ERoundedImage(
@@ -40,7 +42,7 @@ class EProductCardVertical extends StatelessWidget {
                     width: double.infinity,
                     height: double.infinity,
                   ),
-      
+
                   //  Diskon
                   Positioned(
                     top: 6,
@@ -48,71 +50,76 @@ class EProductCardVertical extends StatelessWidget {
                     child: ERoundedContainer(
                       radius: ESizes.sm,
                       backgroundColor: EColors.secondary.withOpacity(0.7),
-                      padding: const EdgeInsets.symmetric(horizontal: ESizes.sm, vertical: ESizes.xs),
-                      child: Text('25%', style: Theme.of(context).textTheme.labelLarge!.apply(color: EColors.black)),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: ESizes.sm,
+                        vertical: ESizes.xs,
+                      ),
+                      child: Text(
+                        '25%',
+                        style: Theme.of(
+                          context,
+                        ).textTheme.labelLarge!.apply(color: EColors.black),
+                      ),
                     ),
                   ),
-      
+
                   // Like
                   Positioned(
                     top: 6,
                     right: 6,
-                    child: ECircularIcon(icon: Iconsax.heart, color: EColors.error, width: 32, height: 32),
+                    child: ECircularIcon(
+                      icon: Iconsax.heart,
+                      color: EColors.error,
+                      width: 32,
+                      height: 32,
+                    ),
                   ),
                 ],
               ),
             ),
-      
+
             const SizedBox(height: ESizes.spaceBtwItems / 2),
-      
+
             Padding(
               padding: EdgeInsets.only(left: ESizes.sm),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const EProductTitleText(title: 'Black Nike Shoes', smallSize: true),
+                  const EProductTitleText(
+                    title: 'Black Nike Shoes',
+                    smallSize: true,
+                  ),
                   const SizedBox(height: ESizes.spaceBtwItems / 2),
-                  Row(
-                    children: [
-                      Text(
-                        'Nike',
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: Theme.of(context).textTheme.labelMedium,
-                      ),
-                      const SizedBox(height: ESizes.xs),
-                      const Icon(Iconsax.verify, color: EColors.primary, size: ESizes.iconXs),
-                    ],
-                  ),
-      
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        '\$40',
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context).textTheme.headlineMedium,
-                      ),
-      
-                      Container(
-                        decoration: const BoxDecoration(
-                          color: EColors.dark,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(ESizes.cardRadiusMd),
-                            bottomRight: Radius.circular(ESizes.cardRadiusMd),
-                          ),
-                        ),
-                        child: const SizedBox(
-                          width: ESizes.iconLg * 1.2,
-                          height: ESizes.iconLg * 1.2,
-                          child: Center(child: Icon(Icons.add, color: EColors.white)),
-                        ),
-                      ),
-                    ],
-                  ),
+                  EBrandTItleWithVerifiedIcon(title: 'Nike',),
                 ],
               ),
+            ),
+
+            const Spacer(),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: ESizes.sm),
+                  child: EProductPrice(price: '40'),
+                ),
+
+                Container(
+                  decoration: const BoxDecoration(
+                    color: EColors.dark,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(ESizes.cardRadiusMd),
+                      bottomRight: Radius.circular(ESizes.cardRadiusMd),
+                    ),
+                  ),
+                  child: const SizedBox(
+                    width: ESizes.iconLg * 1.2,
+                    height: ESizes.iconLg * 1.2,
+                    child: Center(child: Icon(Icons.add, color: EColors.white)),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -120,3 +127,5 @@ class EProductCardVertical extends StatelessWidget {
     );
   }
 }
+
+
